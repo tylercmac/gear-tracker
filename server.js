f056f90a5ab5f0bc8cd4e60900f8b2ea49e50573
ZacharyKathe <55865084+ZacharyKathe@users.noreply.github.com>
2021-04-30 14:54:50 -0700

    setting up server with basic get routs to check if it is working would need to add a main.handlebars, index.handlebars and profile.handlebars

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+require("dotenv").config();
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {
+    maxAge:1000*60*60*2
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public/')));
+app.engine('handlebars', exphbs({defaultLayout: "main"}));
+app.set('view engine', 'handlebars');
+
+
+app.use(routes);
+
+app.get('/', (req,res) => {
+  res.render('index');
+});
+
+
+
+app.get('/profile', (req, res)=>{
+  res.render("profile");   
+})
+
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
