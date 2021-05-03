ec8dd1a5b83985b58057cb46a1cc6d76c12cc002
lbalbrecht <lalbrecht18@wooster.edu>
2021-05-03 10:38:04 -0700

    updated user routes to include bcrypt

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,74 +1,59 @@
 const router = require('express').Router();
-const User = require('../models/User.js');
+const User = require('../models/User');
 const bcrypt = require('bcrypt');
+// const session = require('express-session');
 
-// CREATE new user
+router.get('/sessiondata', (req, res) => {
+    res.json(req.session);
+})
+
+// Create new profile
 router.get('/new', (req, res) => {
-    try {
-        const dbUserData = await User.create({
+    User.create({
             username: req.body.name,
             email: req.body.email,
             password: req.body.password,
-        });
-
-        req.session.save(() => {
-            req.session.loggedIn = true;
-
-            res.status(200).json(dbUserData);
-        });
-    } catch (err) {
+        }).then(newUser => {
+            req.session.user = {
+                id: newUser.id,
+                email: newUser.email,
+                username: newUser.username
+            };
+            res.json(newUser)
+        }).catch (err => {
         console.log(err);
         res.status(500), json(err);
-    }
+    })
 });
 
 // Login to existing profile
-router.post('/login', async (req, res) => {
-    try {
-        const dbUserData = await User.findOne({
-            where: {
-                email: req.body.email,
-            },
-        });
-
-        if (!dbUserData) {
-            res
-                .status(400)
-                .json({ message: 'Incorrect email or password. Please try again!' });
-            return;
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
         }
-
-        const validPassword = await dbUserData.checkPassword(req.body.password);
-
-        if (!validPassword) {
-            res
-                .status(400)
-                .json({ message: 'Incorrect email or password. Please try again!' });
-            return;
+    }).then(foundUser => {
+        if (!foundUser) {
+            req.session.destroy();
+            return res.status(401).send('No account with this email found')
         }
-
-        req.session.save(() => {
-            req.session.loggedIn = true;
-
-            res
-                .status(200)
-                .json({ user: dbUserData, message: 'You are now logged in!' });
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
+        if (bcrypt.compareSync(req.body.password, foundUser.Password)) {
+            req.session.user = {
+                id: foundUser.id,
+                email: foundUser.email,
+                username: foundUser.username
+            }; return res.json(foundUser)
+        } else {
+            req.session.destroy();
+            return res.status(401).send('Username or password is incorrect')
+        }
+    })
 });
 
 // Logout
 router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    } else {
-        res.status(404).end();
-    }
+    req.session.destroy();
+    res.send("Successfully logged out!")
 });
 
 module.exports = router;
