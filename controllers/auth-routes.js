c4c4e6958e9f610c99e1ff91f3906e57e7722f76
tylercmac <tylercmac@gmail.com>
2021-05-03 12:09:26 -0700

    login works

--- controllers/auth-routes.js
+++ controllers/auth-routes.js
@@ -11,45 +11,49 @@ router.get('/sessiondata', (req, res) => {
 router.post('/new', (req, res) => {
     console.log('route reached')
     User.create({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-        }).then(newUser => {
-            req.session.user = {
-                id: newUser.id,
-                email: newUser.email,
-                username: newUser.username
-            };
-            res.json(newUser)
-        }).catch (err => {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(newUser => {
+        req.session.user = {
+            id: newUser.id,
+            email: newUser.email,
+            username: newUser.username
+        };
+        res.json(newUser)
+    }).catch(err => {
         console.log(err);
         res.status(500), json(err);
     })
 });
 
-// Login to existing profile
-router.post('/login', (req, res) => {
+
+router.post('/login', async (req, res) => {
     console.log('route reached!')
-    User.findOne({
-        where: {
-            email: req.body.email
-        }
-    }).then(foundUser => {
+
+    try {
+        const foundUser = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
         if (!foundUser) {
             req.session.destroy();
             return res.status(401).send('No account with this email found')
         }
-        if (bcrypt.compareSync(req.body.password, foundUser.Password)) {
+        if (bcrypt.compareSync(req.body.password, foundUser.password)) {
             req.session.user = {
                 id: foundUser.id,
                 email: foundUser.email,
                 username: foundUser.username
-            }; return res.json(foundUser)
-        } else {
-            req.session.destroy();
-            return res.status(401).send('Username or password is incorrect')
+            };
+            return res.json(foundUser)
         }
-    })
+    } catch (err) {
+        req.session.destroy();
+        return res.status(401).send('Username or password is incorrect')
+
+    }
 });
 
 // Logout
