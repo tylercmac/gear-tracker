c05cbb7e2449cee3a4cb95c4be7933ec5909dd9f
tylercmac <tylercmac@gmail.com>
2021-05-03 10:23:46 -0700

    form handlers for login in scirpt

---
+++ public/js/script.js
@@ -0,0 +1,50 @@
+document.querySelector("#loginForm").addEventListener("submit",event=>{
+  event.preventDefault();
+  const fetchObj = {
+      email:document.querySelector("#loginEmail").value,
+      password:document.querySelector("#loginPassword").value,
+  }
+  console.log(fetchObj);
+  fetch("/login",{
+      method:"POST",
+      body:JSON.stringify(fetchObj),
+      headers:{
+          "Content-Type":"application/json"
+      }
+  }).then(res=>{
+      console.log(res);
+      if(res.ok){
+          console.log("logged in successfully!")
+          location.replace("/dashboard")
+      } else {
+          alert("log in failed!")
+          location.reload();
+      }
+  })
+})
+
+document.querySelector("#signupForm").addEventListener("submit",event=>{
+  event.preventDefault();
+  const fetchObj = {
+      email:document.querySelector("#signupEmail").value,
+      password:document.querySelector("#signupPassword").value,
+      username:document.querySelector("#signupUsername").value,
+  }
+  console.log(fetchObj);
+  fetch("/signup",{
+      method:"POST",
+      body:JSON.stringify(fetchObj),
+      headers:{
+          "Content-Type":"application/json"
+      }
+  }).then(res=>{
+      console.log(res);
+      if(res.ok){
+          console.log("signed up successfully!")
+          location.replace("/dashboard")
+      } else {
+          alert("signup failed!")
+          location.reload();
+      }
+  })
+})
