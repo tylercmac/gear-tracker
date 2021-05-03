f5556f04f6f90a77ec32e3bf863e7f2b2b696345
lbalbrecht <lalbrecht18@wooster.edu>
2021-05-03 10:57:41 -0700

    pushing changes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,11 +4,11 @@ const User = require('../models/User');
 const bcrypt = require("bcrypt");
 
 router.get("/dashboard", (req, res) => {
-  if (!req.session.user) {
-      return res.redirect("/")
-  } else {
-      res.render("dashboard", req.session.user)
-  }
+    if (!req.session.user) {
+        return res.redirect("/")
+    } else {
+        res.render("dashboard", req.session.user)
+    }
 })
 
 module.exports = router;
