5efd5dabf9dab914b8592f3c70647ab088139443
ZacharyKathe <55865084+ZacharyKathe@users.noreply.github.com>
2021-05-06 15:16:15 -0700

    updating branch dont merge

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,62 +1,5 @@
-<<<<<<< HEAD
-
-
-
-
-
-
-
-=======
->>>>>>> 2e5b068b84c220606bd57e3c844b3423212c408f
 fetch(`http://localhost:3001/api/trips/5`, {
     method: 'GET',
-<<<<<<< HEAD
-}).then(function (response) {
-    return response.json();
-  })
-  .then(function (data){
-    console.log(data.GearItems)
-    const labels = data.GearItems.map(item=> item.general_name)
-    console.log("mine string", labels)
-   
-    const values = data.GearItems.map(item=> item.weight_oz)
-    console.log("our string", values)
-    var ctx = document.getElementById('myChart').getContext('2d');
-    const myChart = new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: '# of Votes',
-                data: values,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-<<<<<<< HEAD
-=======
-
->>>>>>> 2e5b068b84c220606bd57e3c844b3423212c408f
-=======
 })
     .then(function (response) {
         return response.json();
@@ -101,7 +44,6 @@ fetch(`http://localhost:3001/api/trips/5`, {
                         beginAtZero: true
 
                     }
->>>>>>> Develop
                 }
             }
         })
@@ -109,12 +51,3 @@ fetch(`http://localhost:3001/api/trips/5`, {
 
 
     })
-
-
-
-
-
-// for (i=0; i< data.GearItems; i++){
-    //     product = data.GearItems[i].general_name
-    //     return product
-    //   }
