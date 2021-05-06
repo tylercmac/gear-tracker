4333749b5f8194d39b73ee7db747fcff9ac6ac16
tylercmac <tylercmac@gmail.com>
2021-05-06 10:29:48 -0700

    full crud

--- controllers/api/gear-routes.js
+++ controllers/api/gear-routes.js
@@ -74,33 +74,16 @@ router.put('/:id', async (req, res) => {
   let tripID = (req.body.id);
   console.log(tripID);
   
-  // attempt 1
-  const tripToUpdate = await Trip.findByPk(tripID)
+  try {
+    const tripToUpdate = await Trip.findByPk(tripID)
 
-  tripToUpdate.addGearItem(req.params.id)
-  tripToUpdate.save();
+    tripToUpdate.addGearItem(req.params.id)
+    tripToUpdate.save();
+    res.status(200).json( {message: 'succes'})
+  } catch (err) {
+    console.log(err);
+  }
 
-  res.json({ 
-    include: tripToUpdate
-  })
-  // // Attempt 2
-  //   try {
-  //     const updatedItem = await GearItem.update({ Trips: [req.body]}, {
-  //       where: {
-  //         id: req.params.id
-  //       },
-  //       include: [{ model: Trip }]
-  //     })
-  //     res.status(200).json(updatedItem)
-  
-  //     if (!updatedItem) {
-  //       res.status(404).json({message: 'item not found'})
-        
-  //     }
-  //   } catch (err) {
-  //     res.status(500).json(err);
-  //   }
-  // })
 })
 
 
